b7cc19202b09f7120bb399b70267f755a5f363f2
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2016-05-02 20:48:24 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo /usr/local/bin/wp cli update --allow-root
-wp package install vccw/wp-cli-scaffold-movefile
+wp package install vccw/wp-cli-scaffold-movefile:@stable
 
 wp scaffold movefile --path=/var/www/vhosts/$(curl -L http://169.254.169.254/latest/meta-data/instance-id)
